75f3a636c25013e06883dc2814d46233efe6bde5
Dallas <>
2021-05-14 07:26:23 +0000

    Pushhhhhh

---
+++ commands/coinflip.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js")
+
+exports.run = async(client, message, args) => {
+  const embed = new Discord.MessageEmbed()
+  if (Math.floor(Math.random() * 2) == 0) {
+      coinResult = "Heads!";
+    } else {
+      coinResult = "Tails!";
+    }
+    embed.setTitle(`It's **${coinResult}**!`)
+    message.reply(embed);
+}
