5f32772725fb2a642e5dedcb63db49cedb2c9b26
Dallas <>
2021-05-15 03:10:52 +0000

    not complete...

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js")
 exports.run = async(client, message, args) => {
   const embed = new Discord.MessageEmbed()
   if (Math.floor(Math.random() * 2) == 0) {
-      coinResult = "Heads!";
-    } else {
-      coinResult = "Tails!";
-    }
-    embed.setTitle(`It's **${coinResult}**!`)
-    message.reply(embed);
+    coinResult = "Heads!";
+  } else {
+    coinResult = "Tails!";
+  }
+  embed.setTitle(`It's **${coinResult}**!`)
+  message.reply(embed);
 }
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+//  ==================================================
 //  ================== APP START =====================
+//  ==================================================
 
 // Package Import/Setup
 const express = require('express')
@@ -6,11 +8,75 @@ const app = express()
 const fs = require('fs')
 const Discord = require('discord.js')
 const client = new Discord.Client()
+import fetch from "node-fetch"
 client.config = {}
 client.config.color = "#37fa3d"
 const port = 3000
 const botVersion = 1
 var prefix = "risa!"
+const yuaApi = (base => async endpoint => {
+    const req = await fetch(`${base}${endpoint}`);
+    const res = await req.json();
+    return res;
+})("https://yuabot.com/weeb/api/v1");
+const rCommands = [
+  "bite",
+  "bully",
+  "cuddle",
+  "highfive",
+  "holdhands",
+  "hug",
+  "kill",
+  "kiss",
+  "lick",
+  "meow",
+  "nom",
+  "nuzzle",
+  "pat",
+  "poke",
+  "pout",
+  "punch",
+  "slap",
+  "smile",
+  "snuggle",
+  "stare",
+  "tickle",
+  "wave",
+]
+
+function returnRoleplayPhrase(roleplayCommand, message) {
+  var rCommandPhrases = [
+    "bite": `${message.author.username} is biting ${message.mentions.users}`
+    "bully": //FINISH PHRASE REPLIES https://yuabot.com/api
+    "cuddle",
+    "highfive",
+    "holdhands",
+    "hug",
+    "kill",
+    "kiss",
+    "lick",
+    "meow",
+    "nom",
+    "nuzzle",
+    "pat",
+    "poke",
+    "pout",
+    "punch",
+    "slap",
+    "smile",
+    "snuggle",
+    "stare",
+    "tickle",
+    "wave": `,
+  ]
+}
+
+function checkRoleplay(message) {
+  for (i = 0; i <= rCommands.length; i++) {
+    if (message.content.startsWith(prefix + rCommands[i]))
+      return rCommands[i]
+  }
+}
 
 // Server Listening
 app.get('/', (req, res) => res.send(`Risa v${botVersion} successfully deployed!`))
@@ -20,7 +86,15 @@ app.listen(port)
 client.login(process.env.DISCORD_TOKEN)
 
 // Main Bot Code
-client.on('message', async message => {})
+client.on('message', async message => {
+  // Roleplay Commands
+  if (checkRoleplay(message)) {
+    const img = yuaApi(`/img/${checkRoleplay(message)}`)
+    const embed = new Discord.MessageEmbed()
+      .setColor(client.config.color)
+      .setTitle(``)
+  }
+})
 
 // When bot is Ready, Log to Console
 client.on('ready', () => {
